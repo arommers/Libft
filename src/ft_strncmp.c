127e642d2f00bdcf32f003ba8461c05768146006
Adri Rommers <arommers@f0r1s6.codam.nl>
2023-10-30 10:57:24 +0100

    corrected strncmp

--- src/ft_strncmp.c
+++ src/ft_strncmp.c
@@ -6,7 +6,7 @@
 /*   By: arommers <arommers@student.codam.nl>         +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2022/10/12 14:06:55 by arommers      #+#    #+#                 */
-/*   Updated: 2023/03/18 22:23:09 by adri          ########   odam.nl         */
+/*   Updated: 2023/10/30 10:55:39 by arommers      ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,7 +20,11 @@ int	ft_strncmp(const char *s1, const char *s2, size_t n)
 	while (i < n && (s1[i] || s2[i]))
 	{
 		if (s1[i] != s2[i])
+		{
 			return ((unsigned char)s1[i] - (unsigned char)s2[i]);
+			i++;
+		}
+		else
 			i++;
 		if (!s1)
 			return (0);
@@ -28,18 +32,17 @@ int	ft_strncmp(const char *s1, const char *s2, size_t n)
 	return (0);
 }
 
-/*
-int	main(void)
-{
-	char	*str1;
-	char	*str2;
-	size_t	i;
 
-	str1 = "bulbasaur";
-	str2 = "bulbasaur";
-	i = 8;
-	printf("%d\n", ft_strncmp(str1, str2, i));
-	printf("%d\n", strncmp(str1, str2, i));
-	return (0);
-}
-*/
+// int	main(void)
+// {
+// 	char	*str1;
+// 	char	*str2;
+// 	size_t	i;
+
+// 	str1 = "bulbasaur";
+// 	str2 = "bulba";
+// 	i = 8;
+// 	printf("%d\n", ft_strncmp(str1, str2, i));
+// 	printf("%d\n", strncmp(str1, str2, i));
+// 	return (0);
+// }
